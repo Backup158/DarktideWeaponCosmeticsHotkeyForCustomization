18ba27f4abc8cbfab668782b5078d28ee1a60200
Dylan Nguyen <d.nguyen.1648@gmail.com>
2025-05-22 22:05:16 -0600

    style: comments. initial commit to test keybinds

--- WeaponCosmeticsHotkeyForCustomization/scripts/mods/WeaponCosmeticsHotkeyForCustomization/WeaponCosmeticsHotkeyForCustomization.lua
+++ WeaponCosmeticsHotkeyForCustomization/scripts/mods/WeaponCosmeticsHotkeyForCustomization/WeaponCosmeticsHotkeyForCustomization.lua
@@ -1,9 +1,11 @@
 local mod = get_mod("WeaponCosmeticsHotkeyForCustomization")
 
+-- Primary: Melee
 mod.open_menu_primary = function()
 	mod.open_menu("melee_weapon")
 end
 
+-- Secondary: Ranged
 mod.open_menu_secondary = function()
 	mod.open_menu("ranged_weapon")
 end
